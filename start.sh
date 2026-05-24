b608d9b7d89e3cf07d4922a5685e27d69c0a1a66
NguyenDucAnh040924 <ducanh24092k4@gmail.com>
2026-05-24 16:16:27 +0700

    thêm address của web local

--- start.sh
+++ start.sh
@@ -16,6 +16,7 @@ source venv/bin/activate
 IP_ADDRESS=$(hostname -I | awk '{print $1}')
 
 echo "[+] IP của máy       : $IP_ADDRESS"
+echo "[+] Web Local     : http://127.0.0.1:5000"
 echo "[+] Local Domain     : http://parking.local:5000"
 echo "[*] ==============================================="
 echo "[*] Ứng dụng đang chạy..."
