735ef35237538e51e0221131fd1c1680f300e8eb
NguyenDucAnh040924 <ducanh24092k4@gmail.com>
2026-05-24 09:59:58 +0700

    thay doi boolean

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ if __name__ == '__main__':
     print("  Student  → john@campus.edu  / student123")
     print("  Staff    → guard@campus.edu / staff123")
     print("=" * 60 + "\n")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
